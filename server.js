14d638c9ba6fccaefde292561265f030bcdcee53
johnpaulfernandez <engr.jfernandez@gmail.com>
2019-03-18 13:28:28 -0700

    Initialize apollo server

---
+++ server.js
@@ -0,0 +1,19 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+  type Todo {
+    task: String
+    completed: Boolean
+  }
+  type Query {
+    getTodo: [Todo]
+  }
+`;
+
+const server = new ApolloServer({
+  typeDefs
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server listening on ${url}`);
+});

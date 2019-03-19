ad59c1cf6af9732f281ac5a7848475fdcb66271f
johnpaulfernandez <engr.jfernandez@gmail.com>
2019-03-18 17:21:02 -0700

    Connect Mongoose database to GraphQL server

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const { ApolloServer, gql } = require("apollo-server");
+const mongoose = require("mongoose");
+
+require("dotenv").config({ path: "variables.env" });
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log("DB connected"))
+  .catch(err => console.error(err));
 
 const todos = [{ task: "Wash car", completed: false }];
 

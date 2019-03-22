79f7ab3385066eb3074da3938c3e8ae81dedf028
johnpaulfernandez <engr.jfernandez@gmail.com>
2019-03-22 10:50:44 -0700

    Update theme

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -2,17 +2,17 @@ import Vue from "vue";
 import Vuetify from "vuetify/lib";
 import "vuetify/src/stylus/app.styl";
 import colors from "vuetify/es5/util/colors";
-
-{
-  primary: "colors.teal.base";
-  secondary: "colors.blue-grey.base";
-  accent: "colors.light-green.base";
-  error: "colors.red.base";
-  warning: "colors.amber.base";
-  info: "colors.blue.base";
-  success: "colors.green.base";
-}
+import "vuetify/dist/vuetify.min.css";
 
 Vue.use(Vuetify, {
+  theme: {
+    primary: colors.teal.base,
+    secondary: "#607d8b", //colors.blue - grey.base,
+    accent: "#8bc34a", //colors.light - green.base,
+    error: colors.red.base,
+    warning: colors.amber.base,
+    info: colors.blue.base,
+    success: colors.green.base
+  },
   iconfont: "md"
 });
